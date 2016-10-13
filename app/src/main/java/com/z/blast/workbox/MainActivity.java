c8ce173cf442fd25878d3c696638b9cc91761218
blastz <touchzh1996@outlook.com>
2016-10-13 20:08:54 +0800

    change MainActivity

--- app/src/main/java/com/z/blast/workbox/MainActivity.java
+++ app/src/main/java/com/z/blast/workbox/MainActivity.java
@@ -6,9 +6,11 @@ import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.graphics.Paint;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -25,80 +27,62 @@ import android.widget.TextView;
 
 import com.baidu.mapapi.SDKInitializer;
 
+import org.w3c.dom.Text;
+
 import java.util.ArrayList;
 
 /**
  * Created by Random on 16.6.12.
  */
 public class MainActivity extends Activity{
-    GridView grid;
+    private GridView grid;
+    private MyAdapter myAdapter;
+    private MyClickListener myClickListener;
     int[] imageIds=new int[]{
-            R.drawable.calendar_128,
-            R.drawable.compass_128,
-            R.drawable.flashlight_128,
-            R.drawable.note_128,
-            R.drawable.ruler_128,
-            R.drawable.map_128,
-            R.drawable.calculator_128,
-            R.drawable.weather_128,
-            R.drawable.gradienter_128,
-            R.drawable.alarmclock_128
+            R.mipmap.calendar_128, R.mipmap.compass_128,
+            R.mipmap.flashlight_128, R.mipmap.note_128,
+            R.mipmap.ruler_128,R.mipmap.map_128,
+            R.mipmap.calculator_128, R.mipmap.weather_128,
+            R.mipmap.gradienter_128, R.mipmap.alarmclock_128
     };
     String[] names=new String[]{
             "Calendar","Compass", "FlashLight",
             "Note", "Ruler", "Map",
-            "Calculator", "Weather","MyGradienter",
-            "MyAlarmClock",
+            "Calculator", "Weather","Gradienter",
+            "AlarmClock",
     };
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        SDKInitializer.initialize(getApplicationContext());
         setContentView(R.layout.activity_main);
-        grid=(GridView)findViewById(R.id.grid01);
-        MyAdapter myAdapter=new MyAdapter();
-        grid.setAdapter(myAdapter);
-        MyClickListener myClickListener=new MyClickListener();
-        grid.setOnItemClickListener(myClickListener);
+        init();
+
     }
     class MyClickListener implements GridView.OnItemClickListener{
         @Override
         public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
             Intent intent;
-            if(position==0){
-                intent=new Intent(MainActivity.this,CalendarActivity.class);
-                MainActivity.this.startActivity(intent);
-            }else if(position==1){
-                intent=new Intent(MainActivity.this,CompassActivity.class);
-                MainActivity.this.startActivity(intent);
-            }else if(position==2){
-                intent=new Intent(MainActivity.this,FlashLightActivity.class);
-                MainActivity.this.startActivity(intent);
-            }else if(position==3){
-                intent=new Intent(MainActivity.this,noteActivity.class);
-                MainActivity.this.startActivity(intent);
-            }else if(position==4){
-                intent=new Intent(MainActivity.this,RulerActivity.class);
-                MainActivity.this.startActivity(intent);
-            }else if(position==5){
-                intent=new Intent(MainActivity.this,MapActivity.class);
-                MainActivity.this.startActivity(intent);
-            }else if(position==6){
-                intent=new Intent(MainActivity.this,Calculator.class);
-                MainActivity.this.startActivity(intent);
-            }else if(position==7){
-                intent=new Intent(MainActivity.this,WeatherActivity.class);
-                MainActivity.this.startActivity(intent);
-            }else if(position==8){
-                intent=new Intent(MainActivity.this,GradienterActivity.class);
-                MainActivity.this.startActivity(intent);
-            }else if(position==9){
-                intent=new Intent(MainActivity.this,AlarmActivity.class);
-                MainActivity.this.startActivity(intent);
+            Class targetClass=null;
+            switch(position){
+                case 0:targetClass=CalendarActivity.class;break;
+                case 1:targetClass=CompassActivity.class;break;
+                case 2:targetClass=FlashLightActivity.class;break;
+                case 3:targetClass=noteActivity.class;break;
+                case 4:targetClass=RulerActivity.class;break;
+                case 5:targetClass=MapActivity.class;break;
+                case 6:targetClass=Calculator.class;break;
+                case 7:targetClass=WeatherActivity.class;break;
+                case 8:targetClass=GradienterActivity.class;break;
+                case 9:targetClass=AlarmActivity.class;break;
+
             }
+            intent=new Intent(MainActivity.this,targetClass);
+            startActivity(intent);
         }
     }
     class MyAdapter extends BaseAdapter{
+        ViewHolder viewHolder;
+        LayoutInflater inflater;
         public int getCount(){
             return 10;
         }
@@ -108,23 +92,32 @@ public class MainActivity extends Activity{
         public long getItemId(int position){
             return position;
         }
-        public View getView(int position,View convertView,ViewGroup parent){
-            LinearLayout line=new LinearLayout(MainActivity.this);
-            line.setOrientation(LinearLayout.VERTICAL);
-            ImageView imageView=new ImageView(MainActivity.this);
-            TextView textView=new TextView(MainActivity.this);
-            textView.setText(names[position]);
-            imageView.setImageResource(imageIds[position]);
-            imageView.setLayoutParams(new ViewGroup.LayoutParams(128, 128));
-            RelativeLayout.LayoutParams layoutParams=new RelativeLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT,
-                    ViewGroup.LayoutParams.WRAP_CONTENT);
-            layoutParams.addRule(RelativeLayout.CENTER_IN_PARENT);
-            textView.setLayoutParams(layoutParams);
-            textView.setId(position);
-            line.setGravity(0x11);
-            line.addView(imageView);
-            line.addView(textView);
-            return line;
+        public View getView(int position,View view,ViewGroup parent){
+            inflater=LayoutInflater.from(MainActivity.this);
+              if(view==null){
+                  view=inflater.inflate(R.layout.griditem,null);
+                  viewHolder=new ViewHolder();
+                  viewHolder.imageView=(ImageView)view.findViewById(R.id.griditem_imageView);
+                  viewHolder.textView=(TextView)view.findViewById(R.id.griditem_textView);
+                  view.setTag(viewHolder);
+              }else{
+                  viewHolder=(ViewHolder)view.getTag();
+              }
+            viewHolder.imageView.setImageResource(imageIds[position]);
+            viewHolder.textView.setText(names[position]);
+            return view;
         }
+    }
+    class ViewHolder{
+        ImageView imageView;
+        TextView textView;
+    }
+    public void init(){
+        SDKInitializer.initialize(getApplicationContext());
+        grid=(GridView)findViewById(R.id.grid01);
+        myAdapter=new MyAdapter();
+        grid.setAdapter(myAdapter);
+        myClickListener=new MyClickListener();
+        grid.setOnItemClickListener(myClickListener);
     }
 }
